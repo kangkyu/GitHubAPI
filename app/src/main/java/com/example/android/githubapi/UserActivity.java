0b7ab0342de6559730598bdf3928280f1cd072f7
Kang-Kyu Lee <kangkyu1111@gmail.com>
2018-02-11 15:15:22 -0800

    UserActivity - fill out TextViews with temp text

--- app/src/main/java/com/example/android/githubapi/UserActivity.java
+++ app/src/main/java/com/example/android/githubapi/UserActivity.java
@@ -2,8 +2,19 @@ package com.example.android.githubapi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class UserActivity extends AppCompatActivity {
+
+    ImageView avatarImg;
+    TextView userNameTV;
+    TextView followersTV;
+    TextView followingTV;
+    TextView login;
+    TextView email;
+    Button ownedRepos;
 
     Bundle extras;
     String newString;
@@ -13,9 +24,26 @@ public class UserActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
 
+        avatarImg = (ImageView) findViewById(R.id.avatar);
+        userNameTV = (TextView) findViewById(R.id.user_name);
+        followersTV = (TextView) findViewById(R.id.followers);
+        followingTV = (TextView) findViewById(R.id.following);
+        login = (TextView) findViewById(R.id.login);
+        email = (TextView) findViewById(R.id.email);
+        ownedRepos = (Button) findViewById(R.id.owned_repos_btn);
+
         extras = getIntent().getExtras();
         newString = extras.getString("STRING_I_NEED");
 
         System.out.println(newString);
+        loadData();
+    }
+
+    private void loadData() {
+        userNameTV.setText("No name provided");
+        followersTV.setText("Followers: 0");
+        followingTV.setText("Following: 0");
+        login.setText("Login: " + newString);
+        email.setText("No email provided");
     }
 }
